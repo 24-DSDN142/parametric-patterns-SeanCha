fd95759097b94b62844d43df3201c99d5cc14194
SeanCha <paultiguna@gmail.com>
2024-07-26 16:45:52 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -18,7 +18,32 @@ function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
+let outRectLoc = 10
+let outRectWH = 20
+  let inRectLoc = outRectLoc + outRectWH/4
+  let inRectWH = outRectWH/2
+let connectAmount = 2
+  
+
+
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
-  rect(40,40,40,40);
+  fill(255)
+  rect(outRectLoc,outRectLoc,outRectWH,outRectWH)
+  fill(0)
+  rect(inRectLoc,inRectLoc,inRectWH,inRectWH)
+
+  //rect(outRectLoc,(outRectLoc+outRectWH),50,100)
+  //rect()
+
+  connecters(5,outRectWH)
+}
+
+function connecters(start,end) {
+  if (start>10) {start=10}
+  for (let i = 0; i < start; i++) {
+    var v = (i+1)/(start+2)
+    var v2 = (v*end)
+    rect(v2+outRectLoc,10,outRectWH/10,outRectWH/10)
+    if (i===10) {break;}
+  }
 }
